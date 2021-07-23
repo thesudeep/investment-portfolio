034dd56a761033a5419cd7e6ebe8b9089cb4a847
unknown <thesudeep@gmail.com>
2021-07-23 09:32:04 -0400

    added lologs

--- src/app/util/logos.ts
+++ src/app/util/logos.ts
@@ -218,6 +218,14 @@ export const logos: any[] =   [
     {
       ticker: 'LPT',
       url: 'https://dynamic-assets.coinbase.com/912c3554aaf15c2298ce3af061d09966e47c3a153d93c75bae3d48e458fe1cc1da9b42886f695c23e052f0b8e3d6e94ff9549beffca9ffbc37f3c5e1f22b8f3e/asset_icons/c8201d4ef2a3226f5e1920e8307fb3a3969926a0a97eb294463cb9e6ceb2ca52.png'
+    },
+    {
+      ticker: 'BTC',
+      url: 'https://dynamic-assets.coinbase.com/e785e0181f1a23a30d9476038d9be91e9f6c63959b538eabbc51a1abc8898940383291eede695c3b8dfaa1829a9b57f5a2d0a16b0523580346c6b8fab67af14b/asset_icons/b57ac673f06a4b0338a596817eb0a50ce16e2059f327dc117744449a47915cb2.png'
+    },
+    {
+      ticker: 'ETH',
+      url: 'https://dynamic-assets.coinbase.com/dbb4b4983bde81309ddab83eb598358eb44375b930b94687ebe38bc22e52c3b2125258ffb8477a5ef22e33d6bd72e32a506c391caa13af64c00e46613c3e5806/asset_icons/4113b082d21cc5fab17fc8f2d19fb996165bcce635e6900f7fc2d57c4ef33ae9.png'
     }
 
 
